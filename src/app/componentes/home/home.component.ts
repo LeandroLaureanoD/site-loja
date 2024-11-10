91a5dba7bfb8aa0eeb24472dfd0cd313961fe4f5
LeandroLaureano <laureanoduraes@gmail.com>
2024-11-09 23:38:35 -0300

    add fotos de categoria e produtos pagina inicial e melhoria do layout

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -12,36 +12,42 @@ export class HomeComponent {
       titulo: 'Produto 1',
       descricao: 'Descrição do produto',
       preco: 'R$30,00',
+      imagemSrc: './assets/img/Mobile/produtos/bolsa.png',
       imagemAlt: 'Image cap'
     },
     {
       titulo: 'Produto 2',
       descricao: 'Descrição do produto',
       preco: 'R$40,00',
+      imagemSrc: './assets/img/Mobile/produtos/calca.png',
       imagemAlt: 'Image cap'
     },
     {
       titulo: 'Produto 3',
       descricao: 'Descrição do produto',
       preco: 'R$30,00',
+      imagemSrc: './assets/img/Mobile/produtos/camiseta.png',
       imagemAlt: 'Image cap'
     },
     {
       titulo: 'Produto 4',
       descricao: 'Descrição do produto',
       preco: 'R$40,00',
+      imagemSrc: './assets/img/Mobile/produtos/jaqueta-jeans.png',
       imagemAlt: 'Image cap'
     },
     {
       titulo: 'Produto 5',
       descricao: 'Descrição do produto',
       preco: 'R$30,00',
+      imagemSrc: './assets/img/Mobile/produtos/oculos.png',
       imagemAlt: 'Image cap'
     },
     {
       titulo: 'Produto 6',
       descricao: 'Descrição do produto',
       preco: 'R$40,00',
+      imagemSrc: './assets/img/Mobile/produtos/tenis.png',
       imagemAlt: 'Image cap'
     },
   ];
@@ -50,26 +56,32 @@ export class HomeComponent {
   categorias = [
     {
       nome: 'Categoria 1',
+      imagemSrc: './assets/img/Mobile/categorias/categoria-camiseta.png',
       imagemAlt: 'Image cap'
     },
     {
       nome: 'Categoria 2',
+      imagemSrc: './assets/img/Mobile/categorias/categoria-casacos.png',
       imagemAlt: 'Image cap'
     },
     {
       nome: 'Categoria 3',
+      imagemSrc: './assets/img/Mobile/categorias/categoria-calcas.png',
       imagemAlt: 'Image cap'
     },
     {
       nome: 'Categoria 4',
+      imagemSrc: './assets/img/Mobile/categorias/categoria-calcados.png',
       imagemAlt: 'Image cap'
     },
     {
       nome: 'Categoria 5',
+      imagemSrc: './assets/img/Mobile/categorias/categoria-bolsas.png',
       imagemAlt: 'Image cap'
     },
     {
       nome: 'Categoria 6',
+      imagemSrc: './assets/img/Mobile/categorias/categoria-oculos.png',
       imagemAlt: 'Image cap'
     },
     // Adicione mais categorias conforme necessário
